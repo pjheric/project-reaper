25edffa8eae57cba209a2ebee1116e617d91e2be
Gage Johnson <GageJohnson@mac.com>
2022-01-24 15:59:20 -0800

    basic movement of 1 char

--- Assets/Scripts/playermovement.cs
+++ Assets/Scripts/playermovement.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class playermovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private ProjectReaper playerControls;
+    [SerializeField]
+    float acceleration;
+    [SerializeField]
+    float maxSpeed;
+    
+
+    private void Awake()
+    {
+        playerControls = new ProjectReaper();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        moveManager();
     }
+
+    void moveManager()
+    {
+        var rb = gameObject.GetComponent<Rigidbody2D>();
+        Vector2 move = playerControls.Player.Move.ReadValue<Vector2>();
+        float maxSpeedLimiter = (maxSpeed-rb.velocity.magnitude)/maxSpeed;
+        if (maxSpeedLimiter < 0)
+            maxSpeedLimiter = 0;
+        rb.AddForce(move * acceleration *1000 * Time.deltaTime * maxSpeedLimiter);
+    }
+    void FixedUpdate()
+    {
+        var rb = gameObject.GetComponent<Rigidbody2D>();
+        print(rb.velocity.magnitude);
+    }
+    // void onMove(InputValue value)
+    // {
+    //     var moveInput = value.Get<Vector2>();
+    //     print("wowo");
+    // }
 }
