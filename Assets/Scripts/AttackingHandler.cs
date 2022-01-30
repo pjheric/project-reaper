008bc308a573898fbe6adeadb8eb463dc4b883d8
Gage Johnson <GageJohnson@mac.com>
2022-01-30 11:59:32 -0800

    attack bug fixes/revision

--- Assets/Scripts/AttackingHandler.cs
+++ Assets/Scripts/AttackingHandler.cs
@@ -7,22 +7,21 @@ public class AttackingHandler : MonoBehaviour
 {
     private AttackingControls attackControls;
 
-    [SerializeField] private GameObject sword;
-
-    private GameObject clonedSword;
-    private Vector3 newRotation;
+    [SerializeField] GameObject sword;
+    [SerializeField] Transform directionObj;
 
     private bool attacking;
 
-    [SerializeField] private float weaponDistance = -1.7f;
-    [SerializeField] private float orbitDegreesPerSec = 180.0f;
+    //[SerializeField] private float weaponDistance = -1.7f;
+    [SerializeField] private float orbitDegreesPerSec;
+    [SerializeField] private float swingTime;
+    float swingTimer;
 
     private void Awake()
     {
         attackControls = new AttackingControls();
         attackControls.PlayerAttack.BasicAttack1.performed += BasicAttackContext => BasicAttack();
-
-        clonedSword = Instantiate(sword);
+        sword.SetActive(true);
     }
 
     private void OnEnable()
@@ -41,6 +40,8 @@ public class AttackingHandler : MonoBehaviour
         Debug.Log("attack!");
 
         attacking = true;
+        //set sword angle back 90 degrees in prep for 18degree attack
+        sword.transform.RotateAround(transform.position, Vector3.forward, -90);
     }
 
     private void Update()
@@ -49,23 +50,28 @@ public class AttackingHandler : MonoBehaviour
         {
             // var v = Quaternion.AngleAxis(Time.time * speed * -10, Vector3.up) * new Vector3(distance, 0, 0);
             // clonedSword.transform.position = transform.position + v;
-
-            clonedSword.transform.position = transform.position + (transform.position - transform.position).normalized * weaponDistance;
-            clonedSword.transform.RotateAround(transform.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
+            swingTimer += Time.deltaTime;
+            sword.transform.RotateAround(transform.position, Vector3.forward, orbitDegreesPerSec * Time.deltaTime);
         }
         if (!attacking)
         {
             ManageWeapon();
+        }
+        if(swingTimer >= swingTime)
+        {
+            swingTimer = 0;
+            attacking=false;
         }
     }
 
     private void ManageWeapon()
     {
         // rotation
-        newRotation = new Vector3(transform.eulerAngles.x, transform.eulerAngles.x, transform.eulerAngles.z + 90);
-        clonedSword.GetComponent<Transform>().eulerAngles = newRotation;
+        float angle = directionObj.localRotation.eulerAngles.z-sword.transform.localRotation.eulerAngles.z;
+        sword.transform.RotateAround(transform.position, Vector3.forward, angle);
+        //clonedSword.GetComponent<Transform>().eulerAngles = newRotation;
 
         // position
-        clonedSword.transform.position = transform.position + transform.TransformDirection(new Vector3(-1.7f, 0, 0));
+        //clonedSword.transform.position = transform.position + transform.TransformDirection(new Vector3(-1.7f, 0, 0));
     }
 }
