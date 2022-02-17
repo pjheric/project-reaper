4fda8112538f1a799e58c26663d9b1a5a8774018
Gage Johnson <GageJohnson@mac.com>
2022-02-16 19:05:51 -0800

    failure screen

---
+++ Assets/Scripts/mainManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class mainManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject locus;
+    [SerializeField]
+    TextMeshProUGUI locusHealthText;
+    [SerializeField]
+    GameObject failureScreen;
+    [SerializeField]
+    TextMeshProUGUI failureText;
+
+    public static bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        locusHealthText.text = locus.GetComponent<Entity>().currentHealth+"/"+locus.GetComponent<Entity>().maxHealth;
+        if(locus.GetComponent<Entity>().currentHealth<50)
+        {
+            locusHealthText.color = Color.red;
+        }
+        if(locus.GetComponent<Entity>().currentHealth<=0 && gameOver == false)
+        {
+            gameOver = true;
+            Time.timeScale = 0.1f;
+            failureScreen.SetActive(true);
+            failureText.gameObject.SetActive(true);
+            StartCoroutine(fadeInFailText());
+            locusHealthText.gameObject.SetActive(false);
+        }
+    }
+    IEnumerator fadeInFailText()
+    {
+        float t = 0.0f;
+        while(t < 3.0f)
+        {
+            yield return new WaitForEndOfFrame();
+            t += Time.unscaledDeltaTime;
+            failureText.color = new Color(failureText.color.r,failureText.color.g,failureText.color.b,t/3);        
+        }
+    }
+}
